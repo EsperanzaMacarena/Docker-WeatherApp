5178412337e956e04272b4e8dd484e31598ddda7
Esperanza Macarena <espemacem@gmail.com>
2020-01-29 12:55:49 +0100

    dockerizando weatherApp

---
+++ WeatherApp/routes/stations.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const middleware = require("../middleware/index");
+const controller = require("../controllers/station");
+
+router.get(
+  "/:id/weather",
+  middleware.ensureAuthenticated,
+  controller.getWeatherByStationId
+);
+router.get(
+  "/:id/weather/from/:from/to/:to",
+  middleware.ensureAuthenticated,
+  controller.getWeatherByStationId
+);
+router.delete(
+  "/:id",
+ //middleware.ensureAuthenticated,
+  controller.deleteStation
+);
+
+module.exports = router;
